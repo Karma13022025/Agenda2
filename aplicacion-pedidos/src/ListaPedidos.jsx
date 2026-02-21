e3ae8dba6b763536cc5b8a4f1fb8427d7c0b80c4
Horacio <balderashoracio93@gmail.com>
2026-02-20 18:00:54 -0600

    Arreglar boton en iphone

--- aplicacion-pedidos/src/ListaPedidos.jsx
+++ aplicacion-pedidos/src/ListaPedidos.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { collection, onSnapshot, query, orderBy, doc, updateDoc } from "firebase/firestore";
-import { db } from '../services/firebase';
+import { db } from '../src/services/firebase';
 
 export default function ListaPedidos() {
   const [todosLosPedidos, setTodosLosPedidos] = useState([]);
